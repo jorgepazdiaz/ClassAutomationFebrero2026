67e52975b3c44d4ec5224de161c2470c61ec3e44
jorgepazdiazdiaz <jorgepazdiaz@gmail.com>
2026-04-06 21:32:31 -0500

    fix: limpiar ejecuciones en yml

--- src/test/java/Support/RunCucumberTest.java
+++ src/test/java/Support/RunCucumberTest.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features ="src/test/resources/Feature",
         glue = "Definitions",
-        tags = "@Prueba",
+        tags = "@Prueba6",
         plugin = {
                 "pretty",
                 "json:target/cucumber-report/cucumber.json"
